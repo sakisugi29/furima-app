eb8334f804cd6db4d2075483ec0e69d698642ab6
saki <s.fryuu1218@gmail.com>
2026-09-14 15:54:51 +0900

    fix: プロフィールの郵便番号・住所を入力必須に変更、ログイン・登録画面のフォント調整

--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -26,8 +26,8 @@ class ProfileRequest extends FormRequest
         return [
             'profile_image' => ['nullable', 'image', 'mimes:jpeg,png'],
             'name'          => ['required', 'max:20'],
-            'postal_code'   => ['nullable', 'regex:/^\d{3}-\d{4}$/'],
-            'address'       => ['nullable'],
+            'postal_code'   => ['required', 'regex:/^\d{3}-\d{4}$/'],
+            'address'       => ['required'],
             'building'      => ['nullable'],
         ];
     }
